9d69ff7d5c2b985a3f156062a3a94caced6c2cee
Hash <hashiramanakamoto0@gmail.com>
2025-08-06 05:36:22 +0000

    Agora ele deixou um buraco

--- src/components/discord-logo-icon.tsx
+++ src/components/discord-logo-icon.tsx
@@ -6,7 +6,7 @@ export function DiscordLogoIcon({ className }: { className?: string }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      stroke="currentColor"
+      fill="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
